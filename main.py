ec628f8ba8b04e10d204166c722e24594d8480a9
Ochir Sanjaadorj <sor.net@gmail.com>
2021-02-11 20:17:41 -0600

    test

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-import matplotlib
-matplotlib.use('Agg')
+import Tkinter
+import sys
+import os
 
-import tkinter as tk
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+#create main window
+master = Tkinter.Tk()
+master.title("tester")
+master.geometry("300x100")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+#make a label for the window
+label1 = Tkinter.Label(master, text='Hellooooo')
+# Lay out label
+label1.pack()
 
-root = tk.Tk()
-app = Application(master=root)
-app.root.mainloop()
+# Run forever!
+master.mainloop()
